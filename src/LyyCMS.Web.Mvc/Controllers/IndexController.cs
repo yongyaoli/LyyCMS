53f285c457660ce2ebc22a5b37a70312a706a894
liyongyao <1016549961@qq.com>
2021-12-25 09:19:43 +0800

    初始化vue 前端项目

---
+++ src/LyyCMS.Web.Mvc/Controllers/IndexController.cs
@@ -0,0 +1,14 @@
+﻿using Abp.AspNetCore.Mvc.Controllers;
+using LyyCMS.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LyyCMS.Web.Controllers
+{
+    public class IndexController : AbpController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
