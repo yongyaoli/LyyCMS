af289119311cfae20306b9f204005c2433678f59
cc <435031783@qq.com>
2023-07-10 17:36:34 +0800

    log 日志

--- src/LyyCMS.Web.Mvc/Controllers/HomeController.cs
+++ src/LyyCMS.Web.Mvc/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Abp.AspNetCore.Mvc.Authorization;
 using LyyCMS.Controllers;
+using Castle.Core.Logging;
 
 namespace LyyCMS.Web.Controllers
 {
     [AbpMvcAuthorize]
     public class HomeController : LyyCMSControllerBase
     {
+
+        //AbpController 已有
+        //public ILogger Logger { get; set; }
+
+        //public HomeController()
+        //{
+        //    Logger = NullLogger.Instance;
+        //}
+
         public ActionResult Index()
         {
+
+            Logger.Info("start home 888 ... ");
+
             return View();
         }
     }
